f7e835b5c45fb660b5e0958aa4482a33935b6d8d
Thanat <bztzn512@gmail.com>
2023-10-21 11:22:40 -0400

    connectToApi

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { Request, Response } from "express";
 import { chats } from "../data/data.js";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.process || 5000;
 
@@ -12,16 +15,12 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Api is Running");
 });
 
-app.get("/api/chats", (req: Request, res: Response) => {
+app.get("/api/chat", (req: Request, res: Response) => {
   res.send(chats);
 });
 
 app.get("/api/chats/:id", (req: Request, res: Response) => {
   console.log(req.params.id);
-});
-
-app.get("/api/chats", (req: Request, res: Response) => {
-  res.send(chats);
 });
 
 app.listen(PORT, () => {
